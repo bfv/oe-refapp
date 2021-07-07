6986fb2e35c0eb632e96d7bded2a0ce14b6ac2a7
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-07-07 07:43:31 +0200

    read JSON (1)

--- refapp/cicd/setup.groovy
+++ refapp/cicd/setup.groovy
@@ -1,6 +1,12 @@
 
+import groovy.json.JsonSlurper
+
 def readDependencies(sourceDir) {
     echo 'readDependencies from ' + sourceDir + '/dependencies.json'
+    def propertyFile = new File(sourceDir + '/dependencies.json')
+    def jsonSlurper = new JsonSlurper()
+    dependencies = jsonSlurper.parse(propertyFile)
+    println(dependencies.bfvlib)
 }
 
 return [
